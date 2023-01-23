151be0df011457a2055ddeff609c05b4a2dceb5e
nityamadhav <madhavarora9654@gmail.com>
2023-01-23 21:22:29 +0530

    build

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8914,7 +8914,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63435" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63228" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
